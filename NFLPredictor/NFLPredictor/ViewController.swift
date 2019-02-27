273ca857ee6fb655adad3d137acc29742430cbe8
Michael Frain <michaelfrain@Michaels-iMac.local>
2019-02-26 22:44:37 -0800

    Add in model and example functions

--- NFLPredictor/NFLPredictor/ViewController.swift
+++ NFLPredictor/NFLPredictor/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var visitingTeamLabel: UILabel!
+    @IBOutlet var homeTeamLabel: UILabel!
+    
+    @IBOutlet var visitingTeamPoints: UITextField!
+    @IBOutlet var visitingTeamYards: UITextField!
+    @IBOutlet var visitingTeamTurnovers: UITextField!
+    
+    @IBOutlet var homeTeamPoints: UITextField!
+    @IBOutlet var homeTeamYards: UITextField!
+    @IBOutlet var homeTeamTurnovers: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        predictHomeScore(sender: nil)
+        predictHomeTeam(sender: nil)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func predictHomeScore(sender: UIButton?) {
+        let model = ScoreRegressor()
+        
+        guard let scoreOutput = try? model.prediction(Visitor: "New England Patriots", Home: "Los Angeles Rams", PtsV: 13.0, YdsV: 407.0, TOV: 1.0, YdsH: 260.0, TOH: 1.0) else {
+            fatalError()
+        }
+        print(scoreOutput.PtsH)
+    }
+    
+    @IBAction func predictHomeTeam(sender: UIButton?) {
+        let model = TeamClassifier()
+        
+        guard let teamOutput = try? model.prediction(Visitor: "New England Patriots", PtsV: 13.0, PtsH: 3.0, YdsV: 407.0, TOV: 1.0, YdsH: 260.0, TOH: 1.0) else {
+            fatalError()
+        }
+        print(teamOutput.Home)
+        print(teamOutput.HomeProbability)
+    }
 }
 
